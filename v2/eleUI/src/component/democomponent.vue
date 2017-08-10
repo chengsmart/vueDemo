fc7680706984b5477f5a4a34ddf876b1271f1d6a
程帅 <tsl-chengshuai@tasly.com>
2017-08-10 15:12:46 +0800

    完成路由的添加和配置，跳转代调试

---
+++ v2/eleUI/src/component/democomponent.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="democomponent">
+        <h1>I am a title.</h1>
+        <a> my name is {{ author }} </a>
+    </div>
+</template>
+
+<script type="text/javascript">
+    export default {
+        data () {
+            return {
+                author: "Smart"
+            }
+        }
+    }
+</script>
+
+<style>
+</style>
--- v2/eleUI/src/main.js
+++ v2/eleUI/src/main.js
@@ -1,7 +1,43 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+import VueResource from 'vue-resource'
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
+//开启debug模式
+Vue.config.debug = true;
+
+Vue.use(VueRouter);
+Vue.use(VueResource);
+
+
+const First = { template: '<div><h2>我是第 1 个子页面</h2></div>' };
+import secondcomponent from './component/secondcomponent.vue'
+
+// 创建一个路由器实例
+// 并且配置路由规则
+const router = new VueRouter({
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {
+            path: '/first',
+            component: First
+        },
+        {
+            path: '/second',
+            component: secondcomponent
+        }
+    ]
 })
+
+// 现在我们可以启动应用了！
+// 路由器会创建一个 App 实例，并且挂载到选择符 #app 匹配的元素上。
+const app = new Vue({
+    router: router,
+    render: h => h(App)
+}).$mount('#app')
+
+// new Vue({
+//     el: '#app',
+//     render: h => h(App)
+// })
