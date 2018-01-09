db29800284d9b4b7ed203bc8931d1c3dc2ce173d
程帅 <tsl-chengshuai@tasly.com>
2018-01-09 09:39:34 +0800

    实现父子组件，父组件传递数值子组件未刷新bug待调试

--- v2/vux/src/views/Tab/Index.vue
+++ v2/vux/src/views/Tab/Index.vue
@@ -6,29 +6,18 @@
                     v-for="(item, index) in tabList"
                     :key="'h' + index"
                     :selected="item.active"
-                    @on-item-click="search(index)">{{item.title}}
+                    @on-item-click="change(index)">{{item.title}}
             </button-tab-item>
         </button-tab>
-        <ul class="my-tab-content"
-            v-for="(item, index) in tabList"
-            :key="'c' + index"
-            v-if="item.active">
-            <li> ID : {{item.data.id}}</li>
-            <li> 标题 : {{item.data.title}}</li>
-            <li> 时间 : {{item.data.createAt | timestampDateFilter}}</li>
-            <li> 作者 : {{item.data.createBy}}</li>
-        </ul>
-        <h1
-                v-for="(item, index) in tabList"
-                :key="'key' + index"
-                v-if="item.active">当前活跃标签页为{{index + 1}}</h1>
+        <my-tab-content :item-id="reqId" v-if="reqId" @listenChild="showMessage"></my-tab-content>
+        <h1>{{message}}</h1>
     </div>
 </template>
 
 <script>
+    import myTabContent from '@/components/TabContent'
     import {XHeader, ButtonTab, ButtonTabItem} from 'vux'
     import { getTabList } from '@/api/tab'
-    import { timestampDateFilter } from '@/filters/index'
 
     export default {
         name: 'Tab',
@@ -47,27 +36,27 @@
                     title:'标签页3',
                     data:[],
                     active:false
-                }]
+                }],
+                reqId : '',
+                message:''
             }
         },
-        components: {XHeader,ButtonTab, ButtonTabItem},
+        components: {myTabContent,XHeader,ButtonTab, ButtonTabItem},
         methods: {
-            search (index = 0) {
+            change (index = 0) {
                 // 全部置空
-                this.tabList.map(item=>{
+                this.tabList.map(item => {
                     item.active = false
                 });
                 // 设置活跃标签页
                 this.tabList[index].active = true;
                 // 获取数据
-                getTabList( index + 1)
-                    .then(data => {
-                        this.tabList[index].data = data;
-                    })
+                this.reqId = String(index + 1);
+                console.log(this.reqId);
+            },
+            showMessage(data){
+                this.message = data;
             }
-        },
-        filters:{
-            timestampDateFilter
         },
         mounted() {
 
@@ -77,21 +66,5 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-    .my-tab-content {
-        display: block;
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-    }
-    .my-tab-content > li {
-        display: block;
-        height: 30px;
-        line-height: 30px;
-        background-color: #9af;
-        padding: 0 20px;
-        color: #333;
-    }
-    .my-tab-content > li + li {
-        margin-top: 5px;
-    }
+
 </style>
