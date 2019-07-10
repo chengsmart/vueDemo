b2fdaf385a2db8014697deddd14bc5902395813d
Smart <chengshuai.tj@gmail.com>
2019-07-10 16:28:30 +0800

    Update variables.js

--- v2/mintUI/src/common/variables.js
+++ v2/mintUI/src/common/variables.js
@@ -4,14 +4,14 @@
  * Author:Smart
  * Description:
  */
-const HOSTNAME = 'http://mtstg.tasly.com/',
-    SSOHOSTNAME = 'http://mtstg.tasly.com/',
-    HOSTNAME_OTC = 'http://mtstg.tasly.com/';
+const HOSTNAME = 'http://demo.xxx.com/',
+    SSOHOSTNAME = 'http://demo.xxx.com/',
+    HOSTNAME_OTC = 'http://demo.xxx.com/';
 const ENV_CONFIG = {
-    OTCLink: HOSTNAME + 'otc/html/',
+    OTCLink: HOSTNAME + 'xxx/html/',
     OTCId:51,
     JKXWId:51
 }
-const BASEURL = HOSTNAME + 'cdm-app/v2/';//网络的url
-const BASEURL_PUBLIC = HOSTNAME + 'cdm-app/public/v2/';//不需要授权的网络url
-const BASEURL_OTC = HOSTNAME_OTC + 'otc-mobile/api/v1/'; // OTC 接口
+const BASEURL = HOSTNAME + 'xxx-app/v2/';//网络的url
+const BASEURL_PUBLIC = HOSTNAME + 'xxx-app/public/v2/';//不需要授权的网络url
+const BASEURL_OTC = HOSTNAME_OTC + 'xxx-mobile/api/v1/'; //  接口
